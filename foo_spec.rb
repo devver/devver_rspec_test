621e1b8e545ec764a25c6d9951014a9b8b0ea60c
Ben Brinckerhoff <ben@devver.net>
2009-07-08 17:57:34 -0600

    Added many more scenarios for RSpec. Now all the basic should be covered

--- foo_spec.rb
+++ foo_spec.rb
@@ -2,9 +2,37 @@ $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
 require 'foo'
 require 'spec'
 
+def passing_spec
+  true.should == true
+end
+
+def failing_spec
+  flunk
+end
+
+def error!
+  raise "Error!"
+end
+
 describe Foo do
 
-  describe "#bar" do
+  describe "basic cases" do
+    
+    it "should pass" do
+      passing_spec
+    end
+
+    it "should fail" do
+      failing_spec
+    end
+
+    it "should be pending" do
+      pending
+    end
+
+  end
+
+  describe "with before(:each)" do
     
     before :each do
       @it = Foo.new
@@ -14,16 +42,108 @@ describe Foo do
       @it.bar.should == "bar"
     end
 
-    it "should be pending" do
-      pending
+    it "should fail" do
+      @it.bar.should == "baz"
     end
 
   end
 
-  describe "#bar" do
+  describe "with before(:all)" do
     
-    it "should fail due to incorrect result" do
-      Foo.new.bar.should == "bar bar"
+    before :all do
+      @it = Foo.new
+    end
+
+    it "should pass" do
+      @it.bar.should == "bar"
+    end
+
+    it "should fail" do
+      @it.bar.should == "baz"
+    end
+
+  end
+
+  describe "with after(:each)" do
+
+    after :each do
+      @it.bar
+    end
+
+    it "should pass" do
+      @it = Foo.new
+      @it.bar.should == "bar"
+    end
+
+    it "should fail" do
+      @it = Foo.new
+      @it.bar.should == "baz"
+    end
+    
+  end
+
+  describe "with after(:all)" do
+
+    after :all do
+      @it.bar
+    end
+
+    it "should pass" do
+      @it = Foo.new
+      @it.bar.should == "bar"
+    end
+
+    it "should fail" do
+      @it = Foo.new
+      @it.bar.should == "baz"
+    end
+    
+  end
+
+  describe "failure in before :each" do
+    
+    before :each do
+      error!
+    end
+    
+    it "should pass" do
+      passing_spec
+    end
+    
+  end
+
+  describe "failure in after :each" do
+    
+    after :each do 
+      error!
+    end
+    
+    it "should pass" do
+      passing_spec
+    end
+
+  end
+
+  describe "failure in before :all" do
+    
+    before :all do
+      error!
+    end
+
+    it "should pass" do
+      passing_spec
+    end
+    
+  end
+
+  describe "failure in after :all" do
+    
+    after :all do
+      error!
+    end
+
+    it "should pass" do
+      passing_spec
     end
     
   end
@@ -35,5 +155,85 @@ describe Foo do
     end
     
   end
+  
+  shared_examples_for "it's using a shared example" do
+    
+    it "should pass with" do
+      passing_spec
+    end
 
+    it "should fail" do
+      failing_spec
+    end
+
+    it "should error" do
+      error!
+    end
+
+    it "should be pending" do
+      pending
+    end
+    
+  end
+
+  describe "something that uses a shared example" do
+    
+    it_should_behave_like "it's using a shared example"
+    
+  end
+
+  context "when in a context" do
+    
+    it "should pass" do
+      passing_spec
+    end
+
+    it "should fail" do
+      failing_spec
+    end
+
+    it "should error" do
+      error!
+    end
+
+    it "should be pending" do
+      pending
+    end
+    
+  end
+
+  context "outside context" do
+
+    before :each do
+      @outer = []
+    end
+
+    context "nested context" do
+      
+      before :each do
+        @outer << "test"
+        @inner = 1
+      end
+      
+      
+      it "should have access to outer" do
+        @outer.should == ["test"]
+      end
+
+      it "should have access to inner" do
+        @inner.should == 1
+      end
+      
+    end
+
+    it "should still have access to outer" do
+      @outer.should == []
+    end
+
+    it "should not have access to inner" do
+      defined?(@inner).should_not be_true
+    end
+
+  end
+  
 end
